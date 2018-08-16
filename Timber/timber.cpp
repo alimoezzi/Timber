54951f7d7fed5fd5009ec053b23966f385315155
s_arme <s.arme@ya.ru>
2018-08-16 18:32:13 +0430

    adding background

--- Timber/timber.cpp
+++ Timber/timber.cpp
@@ -6,6 +6,15 @@ int main() {
 	VideoMode vm(1366, 768);
 
 	RenderWindow window(vm, "Timber!!", Style::Fullscreen);
+	
+	Texture textureBackground;/* creating a texture to hold a graohic on the GPU*/
+	textureBackground.loadFromFile("../graphics/background.png");/* load a graphic to texture*/
+
+	Sprite spriteBackground;
+	spriteBackground.setTexture(textureBackground);/* attach texture to the sprite */
+
+	spriteBackground.setPosition(0, 0);/* set sprite to cover the screen */
+
 	while (window.isOpen()) {
 		if (Keyboard::isKeyPressed(Keyboard::Escape)) {
 			window.close();
@@ -14,7 +23,8 @@ int main() {
 			window.close();
 		}
 		window.clear();
-		window.display();
+		window.draw(spriteBackground);/* draws our game scene */
+		window.display();/*we write to a hidden surface then display it to avoiding glitch*/
 	}
 	return 0;
 }
