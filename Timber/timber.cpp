57674d047bb39af1b0586390e1113198714b8596
s_arme <s.arme@ya.ru>
2018-08-28 23:26:14 +0430

    Moving clouds and bees

--- Timber/timber.cpp
+++ Timber/timber.cpp
@@ -1,11 +1,13 @@
 #include <SFML/Graphics.hpp>
-
+#include <iostream>
 using namespace sf;
 
+void moveSprite(int min,int width, int n, bool &beeActive, float &beeSpeed, int minSpeed, sf::Sprite &beeSprite, sf::Time &dt);
 int main() {
 	VideoMode vm(1366, 768);
+	//VideoMode vm(1280, 1024);
 
-	RenderWindow window(vm, "Timber!!", Style::Fullscreen);
+	RenderWindow window(vm, "Timber!!", Style::Resize);
 	
 	Texture textureBackground;/* creating a texture to hold a graohic on the GPU memory*/
 	textureBackground.loadFromFile("../graphics/background.png");/* load a graphic to texture*/
@@ -56,13 +58,27 @@ int main() {
 	float cloud2Speed = 0.0f;
 	float cloud3Speed = 0.0f;
 
+	//variable to control time itself
+	Clock clock;
+
+
 	while (window.isOpen()) {
 		if (Keyboard::isKeyPressed(Keyboard::Escape)) {
 			window.close();
 		}
-		if (Mouse::isButtonPressed(Mouse::Left)) {
+		if (Mouse::isButtonPressed(Mouse::Right)) {
 			window.close();
 		}
+		//Measure time
+		Time dt = clock.restart();// restart the clock
+
+		//setup the bee
+		moveSprite(300,200,20, beeActive, beeSpeed,200 , beeSprite, dt);
+		//setup the cloud
+		moveSprite(0,150,30, cloud1Active, cloud1Speed,10 , cloudSprite1, dt);
+		moveSprite(0,200,40, cloud2Active, cloud2Speed, 25, cloudSprite2, dt);
+		moveSprite(0,100,50, cloud3Active, cloud3Speed,5 , cloudSprite3, dt);
+
 		window.clear();
 		window.draw(spriteBackground);/* draws our game scene */
 
@@ -81,4 +97,30 @@ int main() {
 		window.display();/*we write to a hidden surface then display it to avoiding glitch*/
 	}
 	return 0;
+}
+
+void moveSprite(int min,int width,int n, bool &beeActive, float &beeSpeed, int minSpeed, sf::Sprite &beeSprite, sf::Time &dt) {
+	if (!beeActive) {
+		//How fast the bee is
+		srand((int)time(0) * n);
+		beeSpeed = (rand() % minSpeed) + minSpeed;
+
+		//How high is the bee
+		srand((int)time(0) * n);
+		float height = (rand() % width)+min;
+		beeSprite.setPosition(1366, height);
+		std::cout << height << std::endl;
+		beeActive = true;
+	}//moving the bee
+	else {
+		beeSprite.setPosition(
+			beeSprite.getPosition().x - (beeSpeed * dt.asSeconds()),
+			beeSprite.getPosition().y
+		);
+		//if the bee reached the right edge of the screen
+		if (beeSprite.getPosition().x<0) {
+			//setup the bee agin
+			beeActive = false;
+		}
+	}
 }
